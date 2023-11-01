48ff4148575451630c30d03830eee34dd52bceaa
eduardo-monita <eduardo.monita.dias@gmail.com>
2023-10-31 22:16:10 -0300

    resolve language problem

--- src/main/settings.py
+++ src/main/settings.py
@@ -123,7 +123,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'pt-BR'
+LANGUAGE_CODE = 'en-us'
 
 LANGUAGES = [
     ("pt-BR", _("Português")),
